0fb05e90939a077f948f9483a74429ec30751d3b
Andre Pereira <andreserafim11@gmail.com>
2024-06-11 21:17:05 -0300

    feat: register student use case

---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,48 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Student } from '../../enterprise/entities/student'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashGenerator } from '../../cryptography/hash-generator'
+import { StutentAlreadyExistsError } from './errors/stutent-already-exists-error'
+
+interface RegisterStudentUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StutentAlreadyExistsError,
+  {
+    student: Student
+  }
+>
+
+@Injectable()
+export class RegisterStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const studentWithSameEmail =
+      await this.studentsRepository.findByEmail(email)
+
+    if (studentWithSameEmail) {
+      return left(new StutentAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const student = Student.create({ email, name, password: hashedPassword })
+
+    this.studentsRepository.create(student)
+
+    return right({ student })
+  }
+}

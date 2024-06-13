b70a3f8a5589a7a8f63aa46bcff7de4dcc8ea62e
Andre Pereira <andreserafim11@gmail.com>
2024-06-12 21:17:34 -0300

    feat: crypto stubs

--- src/domain/forum/application/use-cases/errors/stutent-already-exists-error.ts
+++ src/domain/forum/application/use-cases/errors/stutent-already-exists-error.ts
@@ -1,6 +1,6 @@
 import { UseCaseError } from '@/core/errors/use-case-error'
 
-export class stutentAlreadyExistsError extends Error implements UseCaseError {
+export class StutentAlreadyExistsError extends Error implements UseCaseError {
   constructor(identifier: string) {
     super(`Student "${identifier}" already exists`)
   }

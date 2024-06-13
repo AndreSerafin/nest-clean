12956f46dde71fe201caa9a3476e3a2952e36ffe
Andre Pereira <andreserafim11@gmail.com>
2024-06-13 14:03:43 -0300

    feat: env modules

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,8 +3,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { Module } from '@nestjs/common'
-import { APP_GUARD } from '@nestjs/core'
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'

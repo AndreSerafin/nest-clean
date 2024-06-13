f49388339344b90870683accf0248ab51e1f0e87
Andre Pereira <andreserafim11@gmail.com>
2024-06-12 21:39:26 -0300

    feat: auth and register students tests

---
+++ test/respositories/in-memory-students-repository.ts
@@ -0,0 +1,23 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { Student } from '@/domain/forum/enterprise/entities/student'
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+  public items: Student[] = []
+
+  async findByEmail(email: string) {
+    const students = this.items.find((item) => item.email === email)
+
+    if (!students) {
+      return null
+    }
+
+    return students
+  }
+
+  async create(students: Student) {
+    this.items.push(students)
+
+    DomainEvents.dispatchEventsForAggregate(students.id)
+  }
+}
